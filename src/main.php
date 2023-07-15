0cc23b4dabc5177cb6894a95648c1d07e1ef2e3f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 17:44:20 +1000

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,10 @@
+<?php
+namespace skyss0fly\whois;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+class Main extends PluginBase implements Listener{
+
+
+}
